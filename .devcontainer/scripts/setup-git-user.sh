738cdddb2c82b9c7ce97c962d66cd7f28be619fa
Ivan Brajkovic <ivan.brajkovic@icloud.com>
2025-07-16 18:51:19 +0000

    Refactor setup-git-user script to use local Git configuration instead of global

--- .devcontainer/scripts/setup-git-user.sh
+++ .devcontainer/scripts/setup-git-user.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-USERNAME=$(git config --global user.name)
-USEREMAIL=$(git config --global user.email)
+USERNAME=$(git config user.name)
+USEREMAIL=$(git config user.email)
 
-echo "🔍 Checking Git global config..."
+echo "🔍 Checking Git user config..."
 
 if [[ -n "$USERNAME" && -n "$USEREMAIL" ]]; then
-  echo "✅ Git is already configured:"
+  echo "✅ Git user is already configured:"
   echo "  Name : $USERNAME"
   echo "  Email: $USEREMAIL"
   exit 0
 fi
 
-echo "⚠️  Git global user.name or user.email not set."
+echo "⚠️  Git user.name or user.email not set."
 
 read -rp "Enter Git user.name: " NEW_USERNAME
 read -rp "Enter Git user.email: " NEW_USEREMAIL
@@ -20,6 +20,6 @@ read -rp "Enter Git user.email: " NEW_USEREMAIL
 git config user.name "$NEW_USERNAME"
 git config user.email "$NEW_USEREMAIL"
 
-echo "✅ Git global config set:"
+echo "✅ Git user config set:"
 echo "  Name : $(git config user.name)"
 echo "  Email: $(git config user.email)"
